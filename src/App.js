5db023b42ef375f737585753d8e7f1543de4f4d0
Aaron Reyes <ProdAronBeatz@gmail.com>
2022-05-31 23:37:06 -0500

    HAlf way done throught making the nav bar, it is just making the links more functional

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+    </>
+  );
+};
+
+export default App;
