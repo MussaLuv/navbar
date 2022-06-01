c8b4b4e61caba15d3b269a365fb60f483e1b9c14
Aaron Reyes <ProdAronBeatz@gmail.com>
2022-06-01 15:24:03 -0500

    Finished the project about working the css with javascript so the page being on a smaller screen it would work

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,23 +1,23 @@
 import React from "react";
+import { links, social } from "./Data";
 
 const Sidebar = () => {
   return (
-    <div className="links-container show-container">
-      <ul className="links">
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-        <li>
-          <a href="#">Products</a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <h4>Sidebar</h4>
+      <div className="links-container show-container">
+        <ul className="links">
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 };
 
